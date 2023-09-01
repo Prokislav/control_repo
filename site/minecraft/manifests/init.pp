c9beb604a2906f3d20ca9a58a94d9b2eee8ae8bd
Prokislav <48226066+Prokislav@users.noreply.github.com>
2023-09-01 12:48:57 +0200

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,7 @@ class minecraft {
   file {'/opt/minecraft':
     ensure => directory,
   }
-  file {'opt/minecraft/server.jar':
+  file {'/opt/minecraft/server.jar':
     ensure => file,
     source => 'https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar',
   }
